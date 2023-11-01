94f07de73a76b927f2f40880ba990fb0f97c65e6
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-11-01 10:25:51 +0100

    Finish chats and messages pgTabel

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,1 +1,28 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const userSystemEnum = pgEnum("user_system-enum", ["system", "user"]);
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdgName: text("pdg_name").notNull(),
+  pdfUrl: text("pdf_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  fileKey: text("file_key").notNull(),
+});
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatsId: integer("chat_id").references(() => chats.id),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: userSystemEnum("role").notNull(),
+});
