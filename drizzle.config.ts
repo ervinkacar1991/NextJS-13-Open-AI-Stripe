923e0ad7dab055896ee760bdf6d16253d1131d11
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-11-01 20:19:38 +0100

    Add dropzone, fileupload page

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,10 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
+
 export default {
   driver: "pg",
   schema: "./src/lib/db/schema.ts",
@@ -10,5 +14,3 @@ export default {
     connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config;
-
-// npx drizzle-kit push:pg
